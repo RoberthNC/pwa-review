96d84a69982c98801874265f70415e49e87ff399
Roberto Núñez <robertonc1599@gmail.com>
2025-03-06 09:06:17 -0500

    Sección de Service Worker y Fetch Event terminada

---
+++ 02-service-worker/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).then((resp) => {
+      if (resp.ok) {
+        return resp;
+      } else {
+        return fetch("img/main.jpg");
+      }
+    })
+  );
+});
